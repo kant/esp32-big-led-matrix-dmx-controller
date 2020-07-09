8d8c403234b55fe21fce4de590aac58fa6824a22
Phil Meyer <der-rechner.1234@t-online.de>
2020-07-09 18:27:04 +0200

    configfile added

--- config_reader.py
+++ config_reader.py
@@ -3,5 +3,5 @@ import configparser
 config = configparser.ConfigParser()
 config['settings'] = {'resolution': '230x368', 'color': 'blue'}
 
-configfile = open('example.ini', 'w')
+configfile = open('config.ini', 'w')
 config.write(configfile)
