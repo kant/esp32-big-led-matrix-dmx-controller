1aede29d06e3b06404dde852145dcf5a10608304
Phil Meyer <der-rechner.1234@t-online.de>
2020-08-15 14:11:43 +0200

    cleaned up

--- config_creator.py
+++
@@ -1,7 +0,0 @@
-import configparser
-
-config = configparser.ConfigParser()
-config['settings'] = {'quantity': '4', 'ordinance': '0;1;2;3', 'ordinance_shape': 'rectangle', 'led_ordinance': 'TLS', }
-
-configfile = open('Matrix_Emulator\\config.ini', 'w')
-config.write(configfile)
