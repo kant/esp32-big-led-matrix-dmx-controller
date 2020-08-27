8628b93f4476f86e1da965c13118c0219b64c1a5
Patric Meyer <patric.meyer@freenet.de>
2020-08-27 19:45:35 +0200

    added handling (send receive over TCP) and storing (read/write SPIFFS) of network settings

--- led_panel_firmware/led_panel_firmware.ino
+++ led_panel_firmware/led_panel_firmware.ino
@@ -4,6 +4,9 @@
 #include <SPI.h>
 #include <Ethernet.h>
 #include <EthernetUdp.h>
+#include "esp_system.h"
+#include "FS.h"
+#include "SPIFFS.h"
 
 
 #define NUM_LEDS                                  350
@@ -14,21 +17,44 @@
 #define PERF_MEASUREMENT_PIN                      22        // output used for performance measurement with logic analyzer; will be set, when frame is shown; will be reset approx. 20ms after it has been set
 
 #define RECEIVE_DATA_UDP_PORT                     50000
+#define TCP_SERVER_PORT                           50001
 
 #define FRAME_PERIOD_TOLERANCE                    5         // if the time to show a frame elapsed for not more than FRAME_PERIOD_TOLERANCE ms, the frame is nevertheless directly displayed
 
 
-byte mac[] = {
-  0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xFD
-};
-IPAddress staticIp(192, 168, 1, 101);
-IPAddress myDns(192, 168, 1, 1);
-IPAddress gateway(192, 168, 1, 1);
-IPAddress subnet(255, 255, 0, 0);
+#define NETWORK_SETTINGS_FILENAME                 "/netcfg.bin"
+#define FORMAT_SPIFFS_IF_FAILED                   true
 
-//bool useDhcp = false;
-bool useDhcp = true;
+#define TCP_CMD_GET_NETWORK_SETTINGS              1
+#define TCP_CMD_SET_NETWORK_SETTINGS              2
 
+#define TCP_POSTIVE_ACK                           1
+#define TCP_NOT_ENOUGH_DATA_ACK                   2
+#define TCP_WRONG_MSG_SIZE_ACK                    3
+
+
+#pragma pack(1)
+typedef struct {
+  uint8_t useDhcp;
+  uint8_t ipAddress[4];
+  uint8_t subnetMask[4];
+  uint8_t gateway[4];
+  uint8_t dnsServer[4];
+} networkSettingsFileContentType;
+#pragma pack()
+
+#pragma pack(1)
+typedef struct {
+  uint8_t useDhcp;
+  uint8_t macAddress[6];
+  uint8_t ipAddress[4];
+  uint8_t subnetMask[4];
+  uint8_t gateway[4];
+  uint8_t dnsServer[4];
+} networkSettingsType;
+#pragma pack()
+
+networkSettingsType networkSettings;
 
 CRGB leds[NUM_LEDS];
 
@@ -69,6 +95,7 @@ uint8_t frameRingBufferWriteIndex = 0;
 
 
 EthernetUDP udp;
+EthernetServer tcpServer(TCP_SERVER_PORT);
 
 
 uint32_t perfMeasurementPinSetTime = 0;
@@ -88,9 +115,15 @@ void setup()
   
   initFastLed();
 
+  initSpiffs();
+
+  readNetworkSettingsFromSpiffs();
+  
   initEthernet();
   
   initUdpListener();
+
+  initTcpListener();
 }
 
 void initFrameRingBuffer()
@@ -109,9 +142,108 @@ void initFastLed()
   FastLED.show();
 }
 
+void initSpiffs()
+{
+  Serial.println("Initializing SPIFFS...");
+
+  bool initSucceeded = false;
+  do
+  {
+    Serial.println("  Attempting to initializing SPIFFS...");
+    initSucceeded = SPIFFS.begin(FORMAT_SPIFFS_IF_FAILED);
+    if(!initSucceeded)
+    {
+      delay(1000);
+    }
+  } while(!initSucceeded);
+
+  Serial.println("...SPIFFS successfully initialized");
+}
+
+void readNetworkSettingsFromSpiffs()
+{
+  Serial.println("Read network settings from SPIFFS...");
+
+  networkSettingsFileContentType networkSettingsFileContent;
+  initNetworkSettingsFileContent(networkSettingsFileContent);
+
+  File file = SPIFFS.open(NETWORK_SETTINGS_FILENAME);
+  if(!file || file.isDirectory())
+  {
+    Serial.printf("  could not open file %s\n", NETWORK_SETTINGS_FILENAME);
+    Serial.println("  writing default network settings to SPIFFS");
+    if(!writeNetworkSettingsToSpiffs(networkSettingsFileContent))
+    {
+      Serial.println("  failure during writing default network settings");
+    }
+    copyToNetworkSettings(networkSettingsFileContent);
+    Serial.println("...taking default network settings");
+  }
+  else
+  {
+    uint32_t file_size = file.size();
+    if(file_size != sizeof(networkSettingsFileContent))
+    {
+      Serial.printf("  file has wrong size (%u bytes)\n", file_size);
+      Serial.println("  writing default network settings to SPIFFS");
+      if(!writeNetworkSettingsToSpiffs(networkSettingsFileContent))
+      {
+        Serial.println("  failure during writing default network settings");
+      }
+      copyToNetworkSettings(networkSettingsFileContent);
+      Serial.println("...taking default network settings");
+    }
+    else
+    {
+      file.read((uint8_t*)(&networkSettingsFileContent), sizeof(networkSettingsFileContent));
+      file.close();
+      copyToNetworkSettings(networkSettingsFileContent);
+      Serial.println("...network settings successfully read");
+    }
+  }
+}
+
+void initNetworkSettingsFileContent(networkSettingsFileContentType& networkSettingsFileContent)
+{
+  networkSettingsFileContent = {
+    true,                                   // use DHCP (default)
+    {192, 168, 1, 101},                     // default IP adress if not DHCP is used
+    {255, 255, 255, 0},                     // default subnet mask if not DHCP is used
+    {192, 168, 1, 1},                       // default gateway if not DHCP is used
+    {192, 168, 1, 1}};                      // default DNS server if not DHCP is used
+}
+
+void copyToNetworkSettings(networkSettingsFileContentType& networkSettingsFileContent)
+{
+  networkSettings.useDhcp = networkSettingsFileContent.useDhcp;
+  memcpy(&networkSettings.ipAddress, &networkSettingsFileContent.ipAddress, sizeof(networkSettings.ipAddress));
+  memcpy(&networkSettings.subnetMask, &networkSettingsFileContent.subnetMask, sizeof(networkSettings.subnetMask));
+  memcpy(&networkSettings.gateway, &networkSettingsFileContent.gateway, sizeof(networkSettings.gateway));
+  memcpy(&networkSettings.dnsServer, &networkSettingsFileContent.dnsServer, sizeof(networkSettings.dnsServer));
+}
+
+bool writeNetworkSettingsToSpiffs(networkSettingsFileContentType& networkSettingsFileContent)
+{
+  File file = SPIFFS.open(NETWORK_SETTINGS_FILENAME, FILE_WRITE);
+  if(!file || file.isDirectory())
+  {
+    return false;
+  }
+  else
+  {
+    file.write((uint8_t*)(&networkSettingsFileContent), sizeof(networkSettingsFileContent));
+    file.flush();
+    file.close();
+  }
+  return true;
+}
+
 void initEthernet()
 {
   Serial.println("Initializing ethernet...");
+
+  esp_read_mac(networkSettings.macAddress, ESP_MAC_WIFI_STA);
+  Serial.printf("  Took MAC address from ESP32 WiFi for the ethernet interface: %s\n", macToStr(networkSettings.macAddress).c_str());
 
   Ethernet.init(ETH_CS_PIN);
 
@@ -120,10 +252,10 @@ void initEthernet()
   {
     initEthernetBoard();
 
-    if(useDhcp)
+    if(networkSettings.useDhcp)
     {
       Serial.println("  Attempting to connect over DHCP...");
-      ethernetConnected = Ethernet.begin(mac);
+      ethernetConnected = Ethernet.begin(networkSettings.macAddress);
       if(!ethernetConnected)
       {
         if(Ethernet.hardwareStatus() == EthernetNoHardware)
@@ -136,7 +268,15 @@ void initEthernet()
     else
     {
       Serial.println("  Connecting with static IP address...");
-      Ethernet.begin(mac, staticIp, myDns, gateway, subnet);
+      IPAddress ipAddress = IPAddress(networkSettings.ipAddress);
+      IPAddress subnetMask = IPAddress(networkSettings.subnetMask);
+      IPAddress gateway = IPAddress(networkSettings.gateway);
+      IPAddress dnsServer = IPAddress(networkSettings.dnsServer);
+      Serial.printf("    IP address = %s\n", ipAddress.toString().c_str());
+      Serial.printf("    subnet mask = %s\n", subnetMask.toString().c_str());
+      Serial.printf("    gateway = %s\n", gateway.toString().c_str());
+      Serial.printf("    DNS server = %s\n", dnsServer.toString().c_str());
+      Ethernet.begin(networkSettings.macAddress, ipAddress, dnsServer, gateway, subnetMask);
       delay(2000);
       if(Ethernet.hardwareStatus() == EthernetNoHardware)
       {
@@ -153,6 +293,15 @@ void initEthernet()
   Serial.println("  ...successfully connected.");
   Serial.print("  IP address: ");
   Serial.println(Ethernet.localIP());  
+
+  Serial.println("...ethernet successfully initialized");
+}
+
+String macToStr(uint8_t* macAddressBytes)
+{
+  char baseMacChr[18] = {0};
+  sprintf(baseMacChr, "%02X:%02X:%02X:%02X:%02X:%02X", macAddressBytes[0], macAddressBytes[1], macAddressBytes[2], macAddressBytes[3], macAddressBytes[4], macAddressBytes[5]);
+  return String(baseMacChr);
 }
 
 void initEthernetBoard()
@@ -188,6 +337,13 @@ void initUdpListener()
   Serial.println(Ethernet.localIP());
 
   Serial.println("...UDP listener successfully initialized");
+}
+
+void initTcpListener()
+{
+  Serial.printf("Listen to TCP connections on port %u ...\n", TCP_SERVER_PORT);
+
+  tcpServer.begin();
 }
 
 void receivedUdpPacket(int packetSize)
@@ -448,6 +604,144 @@ void loop()
       }
       break;
   }
+
+  processTcp();
+}
+
+void processTcp()
+{
+  EthernetClient tcpClient = tcpServer.available();
+  if(tcpClient)
+  {
+    
+// DEBUG
+//Serial.printf("TCP client %s connected\n", tcpClient.remoteIP().toString().c_str());
+
+    uint8_t cmd = tcpClient.read();
+    processTcpCmd(tcpClient, cmd);
+    
+    tcpClient.stop();
+  }
+}
+
+void processTcpCmd(EthernetClient& tcpClient, uint8_t cmd)
+{
+  if(cmd == TCP_CMD_GET_NETWORK_SETTINGS)
+  {
+
+// DEBUG
+//Serial.println("sending network settings");
+
+    sendNetworkSettings(tcpClient);
+  }
+  else if(cmd == TCP_CMD_SET_NETWORK_SETTINGS)
+  {
+
+// DEBUG
+//Serial.println("receiving network settings");
+
+    receiveNetworkSettings(tcpClient);
+  }
+}
+
+void sendNetworkSettings(EthernetClient& tcpClient)
+{
+  // send back the current network settings inside the answer to the client command
+  tcpClient.write(sizeof(networkSettings)); // size of the network settings data
+  tcpClient.write((uint8_t*)(&networkSettings), sizeof(networkSettings)); // network settings data
+  tcpClient.flush();
+}
+
+void receiveNetworkSettings(EthernetClient& tcpClient)
+{
+  // receive new network settings
+  int messageSize = tcpClient.read(); // size of the new network settings data
+
+// DEBUG
+//Serial.printf("messageSize = %d\n", messageSize);
+  
+  if(messageSize == sizeof(networkSettingsFileContentType))
+  {
+    uint8_t receivedMessage[sizeof(networkSettingsFileContentType)];
+    if(receiveNetworkSettingsData(tcpClient, &receivedMessage[0]))
+    {
+      // send back a positive acknowledgement inside the answer to the client command
+      tcpClient.write(TCP_POSTIVE_ACK);
+      tcpClient.flush();
+
+      networkSettingsFileContentType receivedNetworkSettings;
+      memcpy(&receivedNetworkSettings, &receivedMessage[0], sizeof(receivedNetworkSettings));
+      
+      dumpReceivedNetworkSettings(receivedNetworkSettings);
+      
+      if(!writeNetworkSettingsToSpiffs(receivedNetworkSettings))
+      {
+        Serial.println("### failure during writing new network settings into SPIFFS");
+      }
+
+      tcpClient.stop();
+      
+      Serial.println("################################### restarting... ###################################");
+      delay(1000);
+      ESP.restart();
+    }
+    else
+    {
+      // send back a negative acknowledgement inside the answer to the client command
+      tcpClient.write(TCP_NOT_ENOUGH_DATA_ACK);
+      tcpClient.flush();
+      
+      Serial.println("### command <set network settings> does not contain enough data");  
+    }
+  }
+  else
+  {
+    // send back a negative acknowledgement inside the answer to the client command
+    tcpClient.write(TCP_WRONG_MSG_SIZE_ACK);
+    tcpClient.flush();
+    
+    Serial.println("### command <set network settings> is followed by wrong message size");  
+  }
+}
+
+bool receiveNetworkSettingsData(EthernetClient& tcpClient, uint8_t* receivedMessage)
+{
+  uint8_t totalBytesRead = 0;
+  bool noDataAvailable = false;
+  
+  while((totalBytesRead < sizeof(networkSettingsFileContentType)) && (!noDataAvailable))
+  {
+    int byteRead = tcpClient.read();
+    
+// DEBUG
+//Serial.printf("byteRead = %d\n", byteRead);
+
+    if(byteRead >= 0)
+    {
+      receivedMessage[totalBytesRead] = byteRead;
+      ++totalBytesRead;
+    }
+    else
+    {
+      noDataAvailable = true;
+    }
+  }
+  
+  return !noDataAvailable;
+}
+
+void dumpReceivedNetworkSettings(networkSettingsFileContentType& receivedNetworkSettings)
+{
+  IPAddress ipAddress = IPAddress(receivedNetworkSettings.ipAddress);
+  IPAddress subnetMask = IPAddress(receivedNetworkSettings.subnetMask);
+  IPAddress gateway = IPAddress(receivedNetworkSettings.gateway);
+  IPAddress dnsServer = IPAddress(receivedNetworkSettings.dnsServer);
+  Serial.println("received new network settings:");
+  Serial.printf("  useDhcp = %u\n", receivedNetworkSettings.useDhcp);
+  Serial.printf("  IP address = %s\n", ipAddress.toString().c_str());
+  Serial.printf("  subnet mask = %s\n", subnetMask.toString().c_str());
+  Serial.printf("  gateway = %s\n", gateway.toString().c_str());
+  Serial.printf("  DNS server = %s\n", dnsServer.toString().c_str());
 }
 
 void showFrame()
