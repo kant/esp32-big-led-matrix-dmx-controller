63bf0693d5aef8b9da0bbb333e31b376c5b3ef01
Phil Meyer <der-rechner.1234@t-online.de>
2020-09-20 19:29:30 +0200

    cleaned up my old shit

--- LEEDMATRXISTUFFF/DipTest/ergertdgtf/ergertdgtf.ino
+++
@@ -1,9 +0,0 @@
-void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(8600);
-}
-
-void loop() {
-  // put your main code here, to run repeatedly:
-  Serial.print("sdfdgdsgdgh");
-}
