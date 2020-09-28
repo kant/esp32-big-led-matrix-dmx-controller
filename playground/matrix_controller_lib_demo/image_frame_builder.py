71d217c0c620c7cbf19f97dc64a5db420ae3a055
Patric_Meyer <patric.meyer@freenet.de>
2020-09-28 11:22:05 +0200

    added meaningful functionality to build pixel data

--- playground/matrix_controller_lib_demo/image_frame_builder.py
+++ playground/matrix_controller_lib_demo/image_frame_builder.py
@@ -2,20 +2,28 @@ import cv2
 
 
 class ImageFrameBuilder(object):
-
-    PANEL_LED_COUNT: int = 350
+    IMAGE_WIDTH: int = 50  # must be multiples of 2
+    IMAGE_HEIGHT: int = 28  # must be multiples of 2
+    PANEL_LED_COUNT: int = (IMAGE_WIDTH * IMAGE_HEIGHT) / 4  # matrix of 2x2 panels is expected
+    # matrix layout:
+    # [0 1]
+    # [2 3]
 
     endpoints: list = []
 
     def __init__(self, endpoints: list):
         self.endpoints = endpoints
 
-    def build_frame(self, image: object, endpoints: list) -> list:
-        down_scaled_image = cv2.resize(image, (50, 28), interpolation=cv2.INTER_AREA)
-        top_left_image = down_scaled_image[0:14, 0:25]
-        top_right_image = down_scaled_image[0:14, 25:50]
-        bottom_left_image = down_scaled_image[14:28, 0:25]
-        bottom_right_image = down_scaled_image[14:28, 25:50]
+    def build_frame(self, image: object) -> list:
+        down_scaled_image = cv2.resize(image, (self.IMAGE_WIDTH, self.IMAGE_HEIGHT), interpolation=cv2.INTER_AREA)
+        top_left_image = down_scaled_image[0:int(self.IMAGE_HEIGHT/2),
+                                           0:int(self.IMAGE_WIDTH/2)]
+        top_right_image = down_scaled_image[0:int(self.IMAGE_HEIGHT/2),
+                                            int(self.IMAGE_WIDTH/2):self.IMAGE_WIDTH]
+        bottom_left_image = down_scaled_image[int(self.IMAGE_HEIGHT/2):self.IMAGE_HEIGHT,
+                                              0:int(self.IMAGE_WIDTH/2)]
+        bottom_right_image = down_scaled_image[int(self.IMAGE_HEIGHT/2):self.IMAGE_HEIGHT,
+                                               int(self.IMAGE_WIDTH/2):self.IMAGE_WIDTH]
         top_left_panel_sub_frame: dict = {
             'pixel_data': self._build_sub_panel_pixel_date(top_left_image),
             'endpoint': self.endpoints[0]
@@ -40,14 +48,19 @@ class ImageFrameBuilder(object):
         return frame
 
     def _build_sub_panel_pixel_date(self, image: object) -> bytearray:
-        # only dummy implementation!
-        return self._build_pixel_data([255, 0, 0])
-
-    def _build_pixel_data(self, color: list) -> bytearray:
         pixel_data: bytearray = bytearray()
-        # following bytes represent pixel data; each pixel consists of 3 bytes for RGB
-        for led in range(self.PANEL_LED_COUNT):
-            pixel_data.append(color[0])
-            pixel_data.append(color[1])
-            pixel_data.append(color[2])
+        for row in range(int(self.IMAGE_HEIGHT/2)):
+            for col in range(int(self.IMAGE_WIDTH/2)):
+                x: int = col
+                if (row % 2) != 0:
+                    # row index is odd (1, 3, 5, 6, ...), leds are mounted from right to left
+                    x = int(self.IMAGE_WIDTH/2) - 1 - col
+                pixel_data.extend(self._get_pixel_color_bytes(image, x, row))
         return pixel_data
+
+    def _get_pixel_color_bytes(self, image: object, x: int, y: int) -> bytearray:
+        pixel_color_bytes: bytearray = bytearray()
+        pixel_color_bytes.append(image[y, x, 2])
+        pixel_color_bytes.append(image[y, x, 1])
+        pixel_color_bytes.append(image[y, x, 0])
+        return pixel_color_bytes
